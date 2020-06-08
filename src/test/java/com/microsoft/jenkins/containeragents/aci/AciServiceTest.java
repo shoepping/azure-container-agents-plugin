afc61198ac923da660c349b35d6e6097fe7cb483
Eugen CRISTEA <TudorEugen.Cristea@post.at>
2020-06-08 15:48:18 +0200

    test

--- src/test/java/com/microsoft/jenkins/containeragents/aci/AciServiceTest.java
+++ src/test/java/com/microsoft/jenkins/containeragents/aci/AciServiceTest.java
@@ -1,18 +1,22 @@
 package com.microsoft.jenkins.containeragents.aci;
 
 import com.google.common.collect.ImmutableList;
+import com.microsoft.azure.AzureEnvironment;
+import com.microsoft.azure.credentials.ApplicationTokenCredentials;
 import com.microsoft.azure.management.Azure;
 import com.microsoft.azure.management.containerinstance.OperatingSystemTypes;
 import com.microsoft.jenkins.containeragents.PodEnvVar;
-import hudson.model.Descriptor;
+import com.microsoft.jenkins.containeragents.remote.LaunchMethodTypeContent;
+import com.microsoft.jenkins.containeragents.util.Constants;
+import com.microsoft.rest.LogLevel;
 import hudson.slaves.RetentionStrategy;
+import jenkins.model.Jenkins;
 import org.apache.commons.lang3.time.StopWatch;
 import org.jenkinsci.plugins.docker.commons.credentials.DockerRegistryEndpoint;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertThat;
@@ -20,23 +24,34 @@ import static org.junit.Assert.assertThat;
 public class AciServiceTest {
 
     public static final String RESOURCE_GROUP_NAME = "";
-    public static final String REGION = "";
     private AciCloud cloud;
     private AciContainerTemplate aciContainerTemplate;
     private AciAgent aciAgent;
-    private Azure azureClient;
     private String SUBSCRIPTION_ID = "";
 
-
-
     @Before
-    public void setup() throws IOException, Descriptor.FormException {
+    public void setup() throws Exception {
+
+        String clientId = "";
+        String tenant = "";
+        String key = "";
+        ApplicationTokenCredentials credentials = new ApplicationTokenCredentials(
+                clientId,
+                tenant,
+                key,
+                AzureEnvironment.AZURE);
+
+        Azure client = Azure
+                .configure()
+                .withLogLevel(LogLevel.NONE)
+                .authenticate(credentials)
+                .withSubscription(SUBSCRIPTION_ID);
+
+
         aciContainerTemplate = Mockito.mock(AciContainerTemplate.class);
         cloud = Mockito.mock(AciCloud.class);
-        azureClient = Mockito.mock(Azure.class);
-        Azure.configure().authenticate(null).
+        Mockito.when(cloud.getAzureClient()).thenReturn(client);
         Mockito.when(cloud.getResourceGroup()).thenReturn(RESOURCE_GROUP_NAME);
-        Mockito.when(azureClient.resourceGroups().getByName(RESOURCE_GROUP_NAME).regionName()).thenReturn(REGION);
         String SSH_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCx1IH/KVuZAHA4dWUg8cfJvuy4DFXrnkh+Oi/hl1mh+LfLRsPym6oPDqhIvyOvNDULJzN3ptgoMmFMZhfbs9uz9wo/PWFExsHHHtgKT6dLk2RZIqRBpHr7nzDNvGGOeQStgquvlzMpc2n8mcbSMdXUKi5TulQ079+jblg4ZXM8QEyI0CeMhigmL2+M8o3N15bXyd9/C0yHDTeQipeqj2JtVruaeeO4b5CJHbloF6WK+nIfEUBxB7IRp+0BjxM7XkTdLq8olbx17mY2c5Lkz9Bl0K3+/N0pyOiNJS4RRlpGTxNamoP4Z27LwFYomY7r2nTdwGByogEqMk//1bQt9XKP jenkins@jenkins.org";
         PodEnvVar podEnvVar = new PodEnvVar("JENKINS_AGENT_SSH_PUBKEY", SSH_KEY);
         DockerRegistryEndpoint dockerRegistryEndpoint = new DockerRegistryEndpoint(null, null);
@@ -55,18 +70,18 @@ public class AciServiceTest {
                 new ArrayList<>(),
                 RetentionStrategy.INSTANCE,
                 "1",
-                "512");
-        aciAgent = new AciAgent(cloud, aciContainerTemplate);
+                "1");
+        aciContainerTemplate.setLaunchMethodType(Constants.LAUNCH_METHOD_SSH);
+        aciContainerTemplate.setLaunchMethodTypeContent(new LaunchMethodTypeContent("", "22"));
 
-
+        aciAgent = Mockito.mock(AciAgent.class);
+        Mockito.when(aciAgent.getNodeName()).thenReturn("ta-template-ujyht");
     }
 
     @Test
     public void testCreateDeployment() throws Exception {
-
-        //Assert.assertEquals("", aciCloud.getCredentialsId());
-        AciService.createDeployment(cloud, aciContainerTemplate, aciAgent, StopWatch.createStarted());
-
+        AciService.createDeployment(
+                cloud, aciContainerTemplate, aciAgent, StopWatch.createStarted(), "jenkins_instance_id");
     }
 
 }
